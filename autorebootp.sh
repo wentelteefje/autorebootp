8d901b9b42909aca1e513b6e8cf27ca428e6e413
Kevin Krone <kevin.krone@fu-berlin.de>
2017-11-25 20:24:46 +0100

    Change logfile name and output

--- autorebootp.sh
+++ autorebootp.sh
@@ -2,8 +2,8 @@
 # Determine number of failed cards, i.e. those with 00.00 H/s
 fc=`stats | egrep "^miner_hashes" | tr ' ' '\n' | grep -c "00.00"`
 # Append to log file
-date | tr '\n' ' ' >> ar.log
-echo $fc  >> ar.log
+date | tr '\n' ' ' >> autorebootp.log
+echo "$fc GPUs not hashing"  >> autorebootp.log
 
 # Reset thermals and reboot if failed cards >= 3 else do nothing
 if [ $fc -ge 3 ]; then
